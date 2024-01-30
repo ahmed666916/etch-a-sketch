bf44f96d680d25f85ab898a7fa785f3908879558
ahmed666916 <ahmed666916@gmail.com>
2024-01-30 19:10:38 +0500

    figuring out a way to make it grid

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
 let squares = [];
 
 // Create and append div elements to the array
-for (let i = 0; i < 31; i++) {
+for (let i = 0; i < 16; i++) {
     let square = document.createElement('div');
     square.textContent = "Ahmed";
     square.classList.add('square');
