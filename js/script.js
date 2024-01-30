049ce4dd826f06c61cc8c36216410b8e603e7141
ahmed666916 <ahmed666916@gmail.com>
2024-01-30 22:25:01 +0500

    making adjustments to display divs in a grid

--- js/script.js
+++ js/script.js
@@ -1,23 +1,46 @@
 
 
 // Create an array to store the div elements
-let squares = [];
+let rows = [];
 
 // Create and append div elements to the array
 for (let i = 0; i < 16; i++) {
-    let square = document.createElement('div');
-    square.textContent = "Ahmed";
-    square.classList.add('square');
-    squares.push(square);
+    let row = document.createElement('div');
+    row.textContent = "Row";
+    row.classList.add('row');
+    rows.push(row);
 }
+
+let columns = [];
+
+for (let i = 0; i < 16; i++) {
+    let column = document.createElement('div');
+    column.textContent = "Column";
+    column.classList.add('column');
+    columns.push(column);
+}
+
 
 // Select the container element from the NodeList
 const container = document.getElementsByClassName('container')[0];
 
+const row_container = document.getElementsByClassName('row-container')[0];
+
+const column_container = document.getElementsByClassName('column-container')[0];
+
+
+
 // Append all div elements to the container
-squares.forEach(square => {
-    container.appendChild(square);
+rows.forEach(row => {
+    row_container.appendChild(row);
 });
+
+
+columns.forEach(column => {
+    column_container.appendChild(column);
+})
+container.appendChild(row_container);
+container.appendChild(column_container);
 
 // Append the container to the body
 document.body.appendChild(container);
