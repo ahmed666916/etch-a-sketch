afd2d3b3f3395f7ccabe150e96657a900cf10131
ahmed666916 <ahmed666916@gmail.com>
2024-01-31 20:15:37 +0500

    added hovering effect

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
 
 const container = document.querySelector('.container');
 
+
+
 for (let i = 0; i < 257; i++) {
     const div = document.createElement('div');
     container.appendChild(div);
+
+    div.addEventListener('mouseover', func, false);
+    
+}
+
+
+
+function func() {
+    this.setAttribute("style", "background-color: blue");
     
 }
