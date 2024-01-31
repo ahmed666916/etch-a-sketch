ef34dace542e557e6deca84be7abb091c1ed6912
ahmed666916 <ahmed666916@gmail.com>
2024-01-31 22:13:07 +0500

    created changeGridSize function

--- js/script.js
+++ js/script.js
@@ -27,6 +27,23 @@ function createGrid(size) {
     }
 }
 
+// Function to handle button click
+function changeGridSize() {
+    // Get user input for the new grid size
+    let newSize = prompt('Enter the number of squares per side (max: 100):');
+
+    // Validate input
+    newSize = parseInt(newSize);
+    if (isNaN(newSize) || newSize <= 0 || newSize > 100) {
+        alert('Please enter a valid number between 1 and 100.');
+        return;
+    }
+
+    // Update gridSize and create a new grid
+    gridSize = newSize;
+    createGrid(gridSize);
+}
+
 function func() {
     this.setAttribute("style", "background-color: blue");
     
